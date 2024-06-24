981f753c1d97e932b0f32ae2d93ca8cb283d23f8
Nadia Salazar <109547635+Nadia056@users.noreply.github.com>
2024-06-24 13:13:06 -0600

    obtener todos los archivos js y css
    incluir archivos css y js de manera automatizada en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import fs from 'fs';
+
+const getFiles = (dir, fileList = []) => {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            fileList = getFiles(path.join(dir, file), fileList);
+        } else {
+            fileList.push(path.join(dir, file));
+        }
+    });
+    return fileList;
+};
+
+const cssFiles = getFiles('resources/css');
+const jsFiles = getFiles('resources/js');
+const input = [...cssFiles, ...jsFiles];
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/custom.js'],
+            input: input,
             refresh: true,
         }),
     ],
