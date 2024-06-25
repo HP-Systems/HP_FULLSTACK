570673a06b2fc314901846e8e329bb17679aae93
Berenice de la Cruz de la Cruz <berenicedlcrz13@gmail.com>
2024-06-24 22:57:02 -0600

    🌟 feat: Users vista empezada

--- resources/views/USERS/users.blade.php
+++ resources/views/USERS/users.blade.php
@@ -3,9 +3,41 @@
 @section('title', 'Usuarios - Hotel Project')
 
 @section('content')
-<div class="height-100 p-5" style="background-color: #EEEEEE">
-    <!-- Contenido específico de la vista de usuarios -->
-    <h4>Usuarios</h4>
-    <!-- Otro contenido -->
+<div class="height-100 p-4" style="background-color: #EEEEEE">
+    <div style="display: flex; justify-content: space-between; align-items: center;">
+        <h4>Gestión de Usuarios</h4>
+        <button type="button" class="btn btn-negro">Agregar Usuario</button>
+    </div>
+    <div class="p-4 m-3" style="background-color: white;">
+        <table class="table table-hover">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            </tr>
+            <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
 </div>
 @endsection
