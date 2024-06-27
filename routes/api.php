7f402f399a5e0e38489d43493791b5869dcddf1a
Nadia Salazar <109547635+Nadia056@users.noreply.github.com>
2024-06-27 17:40:23 -0600

    eliminacion de controlador por bug

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\DESKTOP\DeskController;
-use App\Http\Controllers\MOVIL\InfoController;
 use App\Http\Controllers\MOVIL\MovilController;
 use App\Http\Controllers\WEB\InfoHotelController;
 use App\Http\Controllers\WEB\RoomController;
@@ -34,7 +33,6 @@ Route::prefix('movil')->group(function () {
 
 //Version 1 movil
 Route::prefix('v1/movil')->group(function () {
-    Route::get('/inforamaionInicio', [InfoController::class, 'hotelIndex']);
 });
 
 //DESKTOP ROUTES
