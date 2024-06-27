369cfb0ecd6f40f3f1c7db0afa52167223c22d0d
Nadia Salazar <109547635+Nadia056@users.noreply.github.com>
2024-06-27 13:53:32 -0600

    Implementar endpoints para Obtener información en la app

---
+++ app/Http/Controllers/MOVIL/infoController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Movil;
+
+use App\Http\Controllers\Controller;
+use App\Models\Hotel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class infoController extends Controller
+{
+    public function hotelIndex(Request $request)
+    {
+        try{
+            $hotel = Hotel::first();
+            return response()->json($hotel, 200);
+            
+        }catch(\Exception $e){
+            Log::error($e->getMessage());
+        }
+        catch (\PDOException $e) {
+            Log::error($e->getMessage());
+        }
+
+       
+    }
+}
