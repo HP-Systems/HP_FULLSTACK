fb21dd19328d2075db3577f4b6d61d86c1c60959
Nadia Salazar <109547635+Nadia056@users.noreply.github.com>
2024-06-27 13:38:19 -0600

    Modulo para modificar informacion en la app

---
+++ app/Http/Controllers/WEB/InfoHotelController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\WEB;
+
+use App\Http\Controllers\Controller;
+use App\Models\Hotel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class InfoHotelController extends Controller
+{
+    public function index(Request $request)
+    {
+        try{
+            $hotel = Hotel::first();
+            return view('configuracion.configuracion', ['hotel' => $hotel]);
+            
+        }catch(\Exception $e){
+            Log::error($e->getMessage());
+        }
+        catch (\PDOException $e) {
+            Log::error($e->getMessage());
+        }
+
+       
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            // Buscar el hotel por ID
+            $hotel = Hotel::find($id);
+            if ($hotel == null) {
+                return redirect()->back()->with('error', 'Hotel no encontrado');
+
+            }
+
+            // Diccionario de errores
+            $messages = [
+                'required' => 'El campo :attribute es obligatorio',
+                'string' => 'El campo :attribute debe ser un texto',
+                'email' => 'El campo :attribute debe ser un email válido',
+                'numeric' => 'El campo :attribute debe ser un número',
+                'digits' => 'El campo :attribute debe tener exactamente :digits dígitos',
+                'date_format' => 'El campo :attribute debe tener un formato de hora válido',
+            ];
+
+            $validator = Validator::make($request->all(), [
+                'nombre' => 'required|string',
+                'direccion' => 'required|string',
+                'email' => 'required|email',
+                'telefono' => 'required|numeric|digits:10',
+                'checkin' => 'required|date_format:H:i',
+                'checkout' => 'required|date_format:H:i',
+                'descripcion' => 'required|string',
+
+            ], $messages);
+            if ($validator->fails()) {
+                return back()->withErrors($validator)->withInput();
+            }
+            // Validar los datos
+
+            // Actualizar los datos del hotel
+            $hotel->nombre = $request->nombre;
+            $hotel->direccion = $request->direccion;
+            $hotel->email = $request->email;
+            $hotel->telefono = $request->telefono;
+            $hotel->checkin = $request->checkin;
+            $hotel->checkout = $request->checkout;
+            $hotel->descripcion = $request->descripcion;
+            $hotel->save();
+
+            return back()->with('success', 'Hotel actualizado correctamente');
+
+        } catch (ValidationException $e) {
+            Log::error($e->getMessage());
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+        catch (\PDOException $e) {
+            Log::error($e->getMessage());
+        }
+
+    }
+
+}
--- resources/views/configuracion/configuracion.blade.php
+++ resources/views/configuracion/configuracion.blade.php
@@ -1,11 +1,138 @@
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
 @extends('../home')
+
 
 @section('title', 'Configuracion - Hotel Project')
 
 @section('content')
+@vite('resources/css/info.css')
 <div class="height-100 p-5" style="background-color: #EEEEEE">
     <!-- Contenido específico de la vista de usuarios -->
-    <h4>Configuracion</h4>
-    <!-- Otro contenido -->
+    <h4>Configuracion del Sistema</h4>
+
+  <div class="bg-white rounded-lg shadow-md p-4">
+  @if(isset($hotel->id))
+  <form method="POST" action="{{ route('updateHotel', ['id' => $hotel->id]) }}">
+    @csrf
+    @method('PUT')
+
+      <div class="mb-4">
+        <label for="nombre" class="block font-medium mb-1 ">
+          Nombre 
+        </label>
+        <br>
+        <input
+          id="nombre"
+          name="nombre"
+          type="text"
+          placeholder="Enter hotel name"
+          required=""
+          value="{{ $hotel->nombre ?? '' }}"
+          
+        />
+      </div>
+      <div class="mb-4">
+        <label for="direccion" class="block font-medium mb-1">
+        Direccion
+        </label>
+        <br>
+        <input
+          id="direccion"
+          name="direccion"
+          placeholder="Enter hotel address"
+          required=""
+          type="text"
+          value="{{ $hotel->direccion ?? '' }}"
+        />
+      </div>
+      <div class="mb-4">
+        <label for="email" class="block font-medium mb-1">
+          Email
+        </label>
+        <br>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="Enter hotel email"
+          required=""
+          value="{{ $hotel->email ?? '' }}"
+        />
+      </div>
+      <div class="mb-4">
+        <label for="telefono" class="block font-medium mb-1">
+        Telefono
+        </label>
+        <br>
+        <input
+          id="telefono"
+          name="telefono"
+          type="tel"
+          placeholder="Enter hotel phone number"
+          required=""
+          value="{{ $hotel->telefono ?? '' }}"
+        />
+      </div>
+      <div class="mb-4 grid grid-cols-2 gap-4">
+        <div>
+          <label for="checkin" class="block font-medium mb-1">
+            Check-in
+          </label>
+          <input
+            id="checkin"
+            name="checkin"
+            type="time"
+            step="3600"
+            required=" "
+            value="{{ isset($hotel->checkin) ? (new Carbon\Carbon($hotel->checkin))->format('H:i') : '' }}"
+          />
+        
+          <label for="checkout" class="block font-medium mb-1">
+            Check-out
+          </label>
+          <input
+            id="checkout"
+            name="checkout"
+            type="time"
+            step="3600"
+            required=""
+            value="{{ isset($hotel->checkout) ? (new Carbon\Carbon($hotel->checkout))->format('H:i') : '' }}"
+          />
+        </div>
+      </div>
+      <div class="mb-4">
+        <label for="descripcion" class="block font-medium mb-1">
+        Descripcion
+        </label>
+        <br>
+        <textarea
+          id="descripcion"
+          name="descripcion"
+          placeholder="Enter hotel description"
+          required=""
+          rows="4"
+          
+        >{{ $hotel->descripcion ?? '' }}</textarea>
+      </div>
+      <button type="submit">Guardar</button>
+      
+    </form>
+    @else
+    <p>Hotel no especificado.</p>
+   @endif
+  </div>
+  @vite('resources/js/app.js')
+  @if ($errors->any())
+  <script>
+            swal({
+                title: "Error!",
+                text: "{{ $errors->first() }}",
+                icon: "error",
+            });
+        </script>
+    @endif
+
+    
 </div>
 @endsection
