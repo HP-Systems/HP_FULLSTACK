98d13ba4dd6da2f4313376b26bb80f044b96bf26
Berenice de la Cruz de la Cruz <75514306+Berenice13@users.noreply.github.com>
2024-06-29 12:57:24 -0600

    🌟 feat: /v1/movil/reservas/create

--- app/Http/Controllers/MOVIL/ReservasController.php
+++ app/Http/Controllers/MOVIL/ReservasController.php
@@ -5,19 +5,36 @@ namespace App\Http\Controllers\MOVIL;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon; 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Reserva;
+use App\Models\HabitacionReserva;
 
 class ReservasController extends Controller
 {
     public function createReserva(Request $request){
+        /*Formato esperado
+            {
+                "fecha_entrada" : "2024-07-02",
+                "fecha_salida" : "2024-07-10",
+                "huespedID" : 1,
+                "habitaciones" : [
+                    {
+                        "id" : "1"
+                    },
+                    {
+                        "id" : "5"
+                    }
+                ]
+            }
+        */
         try{
             $validation = Validator::make(
                 $request->all(),
                 [
                     "fecha_entrada" => "required",
                     "fecha_salida" => "required",
-                    "huesped" => "required",
+                    "huespedID" => "required",
                     "habitaciones" => "required",
                 ]
             );
@@ -33,10 +50,39 @@ class ReservasController extends Controller
                 );
             }
 
+            $reserva = Reserva::create([
+                'huespedID' => $request->huespedID,
+                'fecha_entrada' => $request->fecha_entrada,
+                'fecha_salida' => $request->fecha_salida,
+                'status' => 1
+            ]);
+
+            if (is_array($request->habitaciones) && count($request->habitaciones) > 0) {
+                foreach ($request->habitaciones as $habitacion) {
+                    if (isset($habitacion['id'])) {
+                        DB::table('habitaciones_reservas')->insert([
+                            'reservaID' => $reserva->id,
+                            'habitacionID' => $habitacion['id'],
+                        ]);
+                    }
+                }
+            } else {
+                return response()->json(
+                    [
+                        'status' => 400,
+                        'data' => [],
+                        'msg' => 'El campo habitaciones es requerido y debe ser un array no vacío.',
+                        'error' => []
+                    ], 400
+                );
+            }
+
+            $reservaCreada = Reserva::with('habitaciones')->find($reserva->id);
+
             return response()->json(
                 [
                     'status' => 200,
-                    'data' => ["Data de la reserva"],
+                    'data' => $reservaCreada,
                     'msg' => 'Reserva creada con éxito.',
                     'error' => []
                 ], 200
--- database/migrations/2024_06_14_155151_create_reservas_table.php
+++ database/migrations/2024_06_14_155151_create_reservas_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->unsignedBigInteger('huespedID');
             $table->date('fecha_entrada');
             $table->date('fecha_salida');
-            $table->time('hora_entrada');
-            $table->time('hora_salida');
+            $table->time('hora_entrada')->nulleable();
+            $table->time('hora_salida')->nulleable();
             $table->boolean('status')->default(1);
 
             $table->foreign('huespedID')->references('id')->on('huespedes');
