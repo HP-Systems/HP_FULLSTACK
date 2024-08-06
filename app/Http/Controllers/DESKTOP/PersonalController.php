7c3a4fb33040d395c33b79ecf9d338f34be1d208
Nadia Salazar <109547635+Nadia056@users.noreply.github.com>
2024-08-05 20:32:00 -0600

    mensajes

--- app/Http/Controllers/DESKTOP/PersonalController.php
+++ app/Http/Controllers/DESKTOP/PersonalController.php
@@ -239,7 +239,7 @@ class PersonalController extends Controller
                 return response()->json(
                     [
                         'status' => 404,
-                        'msg' => 'Personal no encontrado',
+                        'msg' => 'Personal no encontrado envia el id del usuario ',
                     ]
                 );
             }
